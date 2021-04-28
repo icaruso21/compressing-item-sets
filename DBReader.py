1c20745199d7441344c71aeccc5e5715588874a6
rvaninmunoz22 <60942982+rvaninmunoz22@users.noreply.github.com>
2021-04-28 18:31:58 -0400

    Add files via upload

---
+++ DBReader.py
@@ -0,0 +1,15 @@
+def getDB(filename):
+    '''
+    Read in a dataset and formats it as a list of list [t[i]],
+    where t is a transaction and i is an item  
+
+    '''
+    file = open(filename, "r")
+    db = []
+    for line in file:
+        t = line.split(" ")
+        db.append(t)
+    return db
+
+db = getDB("test.txt")
+print(db)
