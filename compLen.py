976cacf9b386786329f625d7a10c43277e71748d
ncarolan <nicholas.joseph.carolan@gmail.com>
2021-05-02 14:34:14 -0400

    MDL function added

--- compLen.py
+++ compLen.py
@@ -8,16 +8,24 @@ Parameters:
 Returns:
 	length (int): MDL of codeSet on db
 '''
+	# MDL = length (in bits) describing codeSet + length (in bits) describing db encoded with codeSet
 
-	# length = length (in bits) describing codeSet + length (in bits) describing db encoded with codeSet
+	# L(D|H) - Length of dataset given code set
 
+	dbCopy = [set(t) for t in db]
+	itemFreq = [0 for i in codeSet]
 
-	# L(D|H)
-	lDH = 0
+	# Encode db with code set to find itemset frequencies
+	for idx, itemset in enumerate(codeSet):
+		for idx2, transaction in enumerate(dbCopy):
+			if itemset.issubset(transaction):
+				itemFreq[idx] += 1
+				dbCopy[idx2].remove(itemset)
 
+	totalFreq = sum(itemFreq)
+	lDH = -sum([f * log(f/totalFreq) for f in itemFreq])
 
-	# L(H)
-	lH = 0
-
+	# L(H) - length of code set representation
+	lH = sum([len(itemset) for itemset in codeSet]) + len(codeSet)
 
 	return lH + lDH
