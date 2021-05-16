e2c519343de79805bcd6d547985d7f1f903c1577
rvaninmunoz22 <60942982+rvaninmunoz22@users.noreply.github.com>
2021-05-16 12:42:03 -0400

    Add files via upload

---
+++ noise.py
@@ -0,0 +1,17 @@
+def noise(I, codeSet, db):
+    '''
+	Returns a list of freak transactions in the database.
+
+	Parameters:
+		I (list of sets): Alphabet, list of singleton item sets
+		codeSet (list of sets): Coding set of db
+		db (list of lists): List of transactions comprising dataset 
+	Returns:
+		noise (list of lists): list of freak transactions in the database
+	'''
+    noise = []
+    for t in range(0, len(db)):
+        if len(db[t]) < len(codeSet[t]): # if L_S(t) > L_{CS}(t)
+            noise.append(db[t])
+
+    return noise
